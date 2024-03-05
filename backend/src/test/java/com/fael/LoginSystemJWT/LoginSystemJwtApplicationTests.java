3700ef5b27b7ba5b0abc6a022eaf01781c3ded75
rsadr0pyz <rsadr0pyz@gmail.com>
2024-03-05 15:15:28 -0300

    Start of the Project

---
+++ backend/src/test/java/com/fael/LoginSystemJWT/LoginSystemJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fael.LoginSystemJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSystemJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
