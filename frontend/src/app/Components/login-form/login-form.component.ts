beedbde4847b825bc35eb59718dde6a6cfb6dbe1
rsadr0pyz <rsadr0pyz@gmail.com>
2024-03-08 14:23:15 -0300

    Changing the implementation of the login form to its own component

--- frontend/src/app/Components/login-form/login-form.component.ts
+++ frontend/src/app/Components/login-form/login-form.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { LoginDto } from '../../Dtos/LoginDto';
+import { UserLoginService } from '../../Services/user-login.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-login-form',
-  standalone: true,
-  imports: [],
-  templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.css'
+        selector: 'app-login-form',
+        standalone: true,
+        imports: [CommonModule, ReactiveFormsModule, RouterModule],
+        templateUrl: './login-form.component.html',
+        styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
+        loginForm = new FormGroup({
 
+                login: new FormControl("", {
+                        nonNullable: true,
+                        validators: [Validators.required]
+                }),
+                password: new FormControl("", {
+                        nonNullable: true,
+                        validators: [Validators.required]
+                })
+
+        })
+
+        constructor(private loginService: UserLoginService, private router: Router) { }
+
+
+        displayLoginError: boolean = false
+
+        OnSubmit(): void {
+                if (this.loginForm.valid) {
+                        let loginObj: LoginDto = {
+                                login: this.loginForm.controls["login"].value,
+                                password: this.loginForm.controls["password"].value,
+                        }
+
+                        this.loginService.login(loginObj).then(succeed => {
+                                if (succeed) {
+                                        this.displayLoginError = false;
+                                        this.router.navigate(["/home"]);
+                                } else {
+                                        this.displayLoginError = true;
+                                }
+
+                        })
+                }
+        }
 }
--- frontend/src/app/Pages/login-page/login-page.component.ts
+++ frontend/src/app/Pages/login-page/login-page.component.ts
@@ -4,50 +4,16 @@ import { LoginDto } from '../../Dtos/LoginDto';
 import { UserLoginService } from '../../Services/user-login.service';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
+import { LoginFormComponent } from '../../Components/login-form/login-form.component';
 
 @Component({
         selector: 'app-login-page',
         standalone: true,
-        imports: [CommonModule, ReactiveFormsModule, RouterModule],
+        imports: [LoginFormComponent],
         templateUrl: './login-page.component.html',
         styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
-        loginForm = new FormGroup({
 
-                login: new FormControl("", {
-                        nonNullable: true,
-                        validators: [Validators.required]
-                }),
-                password: new FormControl("", {
-                        nonNullable: true,
-                        validators: [Validators.required]
-                })
-
-        })
-
-        constructor(private loginService: UserLoginService, private router: Router){}
-
-
-        displayLoginError: boolean = false
-
-        OnSubmit(): void {
-                if (this.loginForm.valid) {
-                        let loginObj: LoginDto = {
-                                login: this.loginForm.controls["login"].value,
-                                password: this.loginForm.controls["password"].value,
-                        }
-                        
-                        this.loginService.login(loginObj).then(succeed =>{
-                                if(succeed){
-                                        this.displayLoginError = false;
-                                        this.router.navigate(["/home"]);
-                                }else{
-                                        this.displayLoginError = true;
-                                }
-
-                        })
-                }
-        }
 
 }
